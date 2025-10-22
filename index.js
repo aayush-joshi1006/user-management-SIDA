295f9416c98b7b92b41c8114fb0926a85afdfaa6
Aayush Joshi <aayushjoshi1006@gmail.com>
2025-10-22 20:48:10 +0530

    made database connection and started the server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 4040;
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
